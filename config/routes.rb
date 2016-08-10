2f50e74d8697c0ed1c2ddf0a3834d83ef7df740d
Burak <bfidans1@binghamton.edu>
2016-08-10 14:35:48 +0300

    routs as show_place added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root 'places#index'
+	
 	get 'places', to:'places#index'
-	get 'places/:id', to:'places#show'
+	get 'places/:id', to:'places#show', as: "show_place"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
